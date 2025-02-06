6274308cac20cec0088bd942e3c6cbe35525e459
Raffay123 <48173818+Raffay123@users.noreply.github.com>
2025-02-06 20:57:46 +0500

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200", "https://localhost:4200"));
-
 app.UseAuthentication();
 app.UseAuthorization();
 
